8e8285681b46d48e08dcfcb44a3df967a122dbba
cycy <96785236+lovecycy@users.noreply.github.com>
2025-08-05 02:15:58 +0800

    时间格式化处理

--- src/views/updateTime/index.tsx
+++ src/views/updateTime/index.tsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react"
 import { pathPrefix } from "../../utils/config"
 
 export default function UpdateTime() {
-    const [lastUpdateTime, setLastUpdateTime] = useState()
+    const [lastUpdateTime, setLastUpdateTime] = useState<string>()
     useEffect(() => {
         getLastCommitInfo()
     }, [])
@@ -10,7 +10,6 @@ export default function UpdateTime() {
     async function getLastCommitInfo() {
         const res = await fetch(pathPrefix + '/commitInfo.json')
         const data = await res.json()
-        console.log(data);
         setLastUpdateTime(data.commitDate)
     }
 
@@ -31,8 +30,8 @@ export default function UpdateTime() {
             wordSpacing: '0px'
         }}>
             Last updated:
-            <time dateTime="2025-07-29T21:42:16.000Z">
-                {lastUpdateTime}
+            <time dateTime={lastUpdateTime}>
+                {lastUpdateTime?.slice(0,-6)}
             </time>
         </span>
     </>)
